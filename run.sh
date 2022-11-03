8bdf09463e244291d94fbe1d41a2b909700fcb9d
felix <vip.zatebe@mail.ru>
2022-11-03 19:09:04 +0300

    run script updated

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-java -jar "/home/felix/_Programming/Idea_Projects/OOP/HangManGame/lib/server.jar" -c "/home/felix/_Programming/Idea_Projects/OOP/HangManGame/config/sever_config.json" -s "/home/felix/_Programming/Idea_Projects/OOP/HangManGame/target/HangManGame-1.0-SNAPSHOT.jar" -api "/home/felix/_Programming/Idea_Projects/OOP/HangManGame/lib/javax.servlet-api-1.0.jar"
+path=/home/felix/_Programming/Idea_Projects/OOP/HangManGame
+
+java -jar "$path/lib/server.jar" -c "$path/config/sever_config.json" -s "$path/target/HangManGame-1.0-SNAPSHOT.jar" -api "$path/lib/javax.servlet-api-1.0.jar"
