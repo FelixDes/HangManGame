f0e8e264780674080486454e07ac8b9de541c4fc
felix <vip.zatebe@mail.ru>
2022-11-03 18:23:06 +0300

    Screen splash added

--- frontend/src/components/HangMan.vue
+++ frontend/src/components/HangMan.vue
@@ -1,35 +1,49 @@
 <template>
-  <div class="main_container">
-    <div class="draw_container card">
-      <DrawGallows ref="drawGallows" :state="state"/>
-    </div>
-    <div class="menu_container card">
-      <div class="letter_container card">
-        <div class="letter_sequence">
-          <span class="letter" v-for="(letter, index) in letters" :key="index">{{ letter }}</span>
+  <div>
+    <div class="main_container">
+      <div class="draw_container card">
+        <DrawGallows ref="drawGallows" :state="state"/>
+      </div>
+      <div class="menu_container card">
+        <div class="letter_container card">
+          <div class="letter_sequence">
+            <span class="letter" v-for="(letter, index) in letters" :key="index">{{ letter }}</span>
+          </div>
+          <div class="already_tried">
+            <span v-for="(letter, index) in alreadyTried" :key="index">{{ letter }}</span>
+          </div>
         </div>
-        <div class="already_tried">
-          <span v-for="(letter, index) in alreadyTried" :key="index">{{ letter }}</span>
+        <div class="input_menu">
+          <div class="card input_letter_container">
+            <input class="input_letter" maxlength="1" v-model="input" onkeydown="return /[a-z]/i.test(event.key)"
+                   v-on:keyup.enter="sendLetter">
+            <div :class="input ? 'button' : 'disabled button'" @click="sendLetter"><span>Enter letter</span></div>
+          </div>
+          <div class="button" @click="reset"><span>RESET</span></div>
         </div>
       </div>
-      <div class="input_menu">
-        <div class="card input_letter_container">
-          <input class="input_letter" maxlength="1" v-model="input" onkeydown="return /[a-z]/i.test(event.key)">
-          <div :class="input ? 'button' : 'disabled button'" @click="sendLetter"><span>Enter letter</span></div>
-        </div>
-        <div class="button" @click="reset"><span>RESET</span></div>
-      </div>
     </div>
+    <ScreenSplash class="splash" v-if="winFlag !== 'IN_PROCESS'">
+      <template v-if="winFlag === 'WIN'">
+        <span>You won!</span>
+        <div class="button" @click="reset"><span>Restart?)</span></div>
+      </template>
+      <template v-else-if="winFlag === 'FAIL'">
+        <span>You failed!</span>
+        <div class="button" @click="reset"><span>Restart?</span></div>
+      </template>
+    </ScreenSplash>
   </div>
 </template>
 
 <script>
 import DrawGallows from "@/components/DrawGallows";
 import axios from "axios";
+import ScreenSplash from "@/components/ScreenSplash";
 
 export default {
   name: "HangMan",
-  components: {DrawGallows},
+  components: {ScreenSplash, DrawGallows},
   data() {
     return {
       url: 'http://localhost:8082/Hangman',
@@ -38,6 +52,9 @@ export default {
       state: 0,
       letters: [],
       alreadyTried: [],
+      winFlag: [],
+
+      splash: false
     }
   },
   mounted() {
@@ -47,14 +64,20 @@ export default {
     reset() {
       axios.post(this.url).then(resp => this.fetchData(resp)).catch(error => console.log(error))
       this.$refs.drawGallows.redraw()
+      this.splash = false
     },
     sendLetter() {
+      if (this.winFlag !== 'IN_PROCESS') {
+        return
+      }
       axios.get(this.url + '?letter=' + this.input).then(resp => this.fetchData(resp)).catch(error => console.log(error))
+      this.input = null
     },
     fetchData(resp) {
       this.letters = resp.data.letters
       this.state = resp.data.state
       this.alreadyTried = resp.data.alreadyTried
+      this.winFlag = resp.data.winFlag
     }
   }
 }
@@ -120,6 +143,15 @@ export default {
 .letter_container {
   display: flex;
   flex-direction: column;
+}
+
+.splash {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  font-size: xx-large;
 }
 
 </style>
---
+++ frontend/src/components/ScreenSplash.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="splash">
+    <slot>
+
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ScreenSplash",
+}
+</script>
+
+<style scoped>
+.splash {
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  height: 100vh;
+  width: 100vw;
+
+  background-color: #9d9d9d;
+
+  opacity: 0.8;
+}
+</style>
